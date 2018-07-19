86dc8ea88ad68a966514c3388b892179d003c5ce
hoppenic <hoppenic@gmail.com>
2018-07-19 14:43:31 -0500

    update

--- CoffeeStore/Controllers/AccountController.cs
+++ CoffeeStore/Controllers/AccountController.cs
@@ -10,6 +10,17 @@ namespace CoffeeStore.Controllers
 {
     public class AccountController : Controller
     {
+        //SignInManager provides the APIs for user sign in
+        SignInManager<IdentityUser> _signInManager;
+
+        //this is the constructor for the AccountController
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+
+        }
+
+
         public IActionResult Index()
         {
             return View();
@@ -26,18 +37,70 @@ namespace CoffeeStore.Controllers
         //Responds on post Account/Register
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Register(Models.RegisterViewModel model)
+        public IActionResult Register(RegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
-                //TODO create account and log user in
-                return RedirectToAction("Index", "Home");
+                IdentityUser newUser = new IdentityUser(model.UserName);
+
+                IdentityResult creationResult = _signInManager.UserManager.CreateAsync(newUser).Result;
+                if (creationResult.Succeeded)
+                {
+                    IdentityResult passwordResult = _signInManager.UserManager.AddPasswordAsync(newUser, model.Password).Result;
+                    if (passwordResult.Succeeded)
+                    {
+                        _signInManager.SignInAsync(newUser, false).Wait();
+                        return RedirectToAction("Index", "Home");
+                        
+                    }
+                    else
+                    {
+                        foreach (var error in creationResult.Errors)
+                        {
+                            ModelState.AddModelError(error.Code, error.Description);
+                        }
+                        
+                        
+                    }
+                    
+                }
+                
             }
+
             return View();
 
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignIn(SignInViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
 
+                IdentityUser existingUser = this._signInManager.UserManager.FindByNameAsync(model.UserName).Result;
+                if (existingUser != null)
+                {
+                    Microsoft.AspNetCore.Identity.SignInResult passwordResult = this._signInManager.CheckPasswordSignInAsync(existingUser, model.Password, false).Result;
+                    if (passwordResult.Succeeded)
+                    {
+                        this._signInManager.SignInAsync(existingUser, false).Wait();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("PasswordIncorrect", "Username or Password is incorrect.");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("UserDoesNotExist", "Username or Password is incorrect.");
+
+                }
+            }
+            return View();
+
+        }
 
     }
 }
---
+++ CoffeeStore/Models/SignInViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeStore.Models;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CoffeeStore.Models
+{
+    public class SignInViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
