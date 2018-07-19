ec07ce07eea1a3624c8907e1aed16621fb8f3020
hoppenic <hoppenic@gmail.com>
2018-07-19 13:37:53 -0500

    Register

---
+++ CoffeeStore/Models/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using CoffeeStore.Models;
+
+namespace CoffeeStore.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+
+    }
+}
